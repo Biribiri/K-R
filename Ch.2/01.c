8d18b6ebf05315d46a078be29efb006c84c1dd4d
Biribiri <darknova0@hotmail.com>
2011-07-21 19:08:36 -0500

    rewrote Ex. 01

--- Ch.2/01.c
+++ Ch.2/01.c
@@ -6,23 +6,35 @@ floating-point types.  */
 #include <stdio.h>
 #include <limits.h>
 
+int powt(int x);
+
 int main(void){
 	
-	printf("Size of char %d\n", CHAR_BIT);
-	printf("Size of char max %d\n", UCHAR_MAX);
-	printf("Size of char min %d\n", SCHAR_MIN);
-	printf("Size of int max %d\n", INT_MAX);
-	printf("Size of int min %d\n", INT_MIN);
-	printf("Size of long max %ld\n", LONG_MAX);
-	printf("Size of long min %ld\n", LONG_MIN);
-	printf("Size of signed char max %d\n", SCHAR_MAX);
-	printf("Size of signed char min %d\n", SCHAR_MIN);
-	printf("Size of short max %d\n", SHRT_MAX);
-	printf("Size of short min %d\n", SHRT_MIN);
-	printf("Size of insined char %u\n", UCHAR_MAX);
-	printf("Size of unsigned int %u\n", UINT_MAX);
-	printf("Size of unsigned long %lu\n", ULONG_MAX);
-	printf("Size of unsigned short %u\n", USHRT_MAX);
+	//size of signed and unsigned should be the same
+	printf("Size of char:\t%d bytes\n", sizeof(unsigned char));
+	printf("Size of int:\t%d bytes\n", sizeof(unsigned int));
+	printf("Size of long:\t%d bytes\n", sizeof(unsigned long));
+	printf("Size of short:\t%d bytes\n", sizeof(unsigned short));
+	putchar('\n');
+	printf("Max value of unsinged char:\t%d \n", powt(sizeof(unsigned char)));
+	printf("Max value of unsinged int:\t%d \n", powt(sizeof(unsigned int)));
+	printf("Max value of unsinged long:\t%d \n", powt(sizeof(unsigned long)));
+	printf("Max value of unsinged short:\t%d \n", powt(sizeof(unsigned short)));
+	putchar('\n');
+	printf("Range of singed char:\t%d \n", powt(sizeof(signed char))/2);
+	printf("Range of singed int:\t%d \n", powt(sizeof(signed int))/2);
+	printf("Range of singed long:\t%d \n", powt(sizeof(signed long))/2);
+	printf("Range of singed short:\t%d \n", powt(sizeof(signed short))/2);
 
 	return 0;
+}
+
+int powt(int n){
+	unsigned long i = 1;
+
+	n *= 8;
+	while(n-- > 0)
+		i *= 2;
+
+	return i-1;
 }
